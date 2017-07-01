bf8b23290ea788814dcebdb491f38b4c7bb4edb9
Suraj <ss0193@uah.edu>
2017-07-01 12:44:06 -0500

    CUDA with Windows form application created.

---
+++ GECBIR/mainWindow.cpp
@@ -0,0 +1,17 @@
+#include "mainWindow.h"
+
+namespace GECBIR {
+
+
+	using namespace System;
+
+
+	[STAThread]
+	void main(array<String^>^ arg) {
+		Application::EnableVisualStyles();
+		Application::SetCompatibleTextRenderingDefault(false);
+
+		mainWindow form;
+		Application::Run(%form);
+	}
+}
