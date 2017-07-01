3581f912bd15cf63ce70228929ebc4b86caa0018
Suraj <ss0193@uah.edu>
2017-07-01 12:48:08 -0500

    Added exe files to gitignore.

--- GECBIR/mainWindow.h
+++ GECBIR/mainWindow.h
@@ -52,11 +52,11 @@ namespace GECBIR {
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(170, 121);
+			this->button1->Location = System::Drawing::Point(106, 127);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(75, 23);
 			this->button1->TabIndex = 0;
-			this->button1->Text = L"button1";
+			this->button1->Text = L"RunCUDA";
 			this->button1->UseVisualStyleBackColor = true;
 			this->button1->Click += gcnew System::EventHandler(this, &mainWindow::button1_Click);
 			// 
@@ -69,6 +69,7 @@ namespace GECBIR {
 			this->Name = L"mainWindow";
 			this->Text = L"mainWindow";
 			this->ResumeLayout(false);
+
 		}
 #pragma endregion
 
